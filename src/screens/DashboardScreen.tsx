93f0fdf1ee38035d680850fc48c9ea3179d1a70c
Casey Li <casey.li@gmail.com>
2021-01-29 20:54:03 -0500

    Added placeholder for dashboard wbht.

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import SignedInContext from '../contexts/SignedInContext';
-import { SafeAreaView } from 'react-native';
-import { Layout } from '@ui-kitten/components';
+import { SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+import { Layout, Card, Text } from '@ui-kitten/components';
 import ScreenHeader from '../components/ScreenHeader';
 import { DashboardScreenProps } from '../utilities/types_and_interfaces';
 
@@ -13,7 +13,29 @@ export default function DashboardScreen({navigation}: DashboardScreenProps) {
     <Layout>
       <SafeAreaView>
         <ScreenHeader title='Dashboard' navigation={navigation} />
+        <ScrollView>
+          <Card style={styles.card}appearance='outline'>
+            <Text style={styles.bhtNumber}>?</Text>
+            <Text style={styles.bhtCaption}>WBHT</Text>
+          </Card>
+        </ScrollView>
       </SafeAreaView>
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 10,
+    alignItems: 'center',
+  },
+  bhtNumber: {
+    textAlign: 'center',
+    fontSize: 80,
+    fontWeight: 'bold',
+  },
+  bhtCaption: {
+    textAlign: 'center',
+    fontSize: 32,
+  }
+});
