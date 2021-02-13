e8bce4e7c00c7c809daf9ec667e29d0d3513f3cd
Casey Li <casey.li@gmail.com>
2021-02-13 16:51:25 -0500

    Basic freshbooks api client built.

--- src/components/WeeklyBillableHoursProgress.tsx
+++ src/components/WeeklyBillableHoursProgress.tsx
@@ -3,69 +3,47 @@ import { StyleSheet } from 'react-native';
 import LottieView from 'lottie-react-native';
 import ProgressCircle from 'react-native-progress-circle'
 import { Button, Card, Spinner, Text, useTheme } from '@ui-kitten/components';
-import * as AppAuth from 'expo-app-auth';
-import * as Linking from 'expo-linking';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import freshbooks_api_client from '../utilities/freshbooks_api_client';
 
 function WeeklyBillableHoursProgress() {
   let animation: LottieView | null;
   const theme = useTheme();
 
-  const [checkingFreshbooksAuth, setCheckingFreshbooksAuth] = useState(true);
-  const [freshbooksAuthCredentials, setFreshbooksAuthCredentials] = useState(true);
+  const [checkingFreshbooksAuth, setCheckingFreshbooksAuth] = useState(false);
+  const [launchingFreshbooksLogin, setLaunchingFreshbooksLogin] = useState(false);
+  const [freshbooksAuthorized, setFreshbooksAuthorized] = useState(false);
 
   const currentWeeklyBillableHours = 19;
   const weeklyBillableHoursTarget = 24;
   const percentage = currentWeeklyBillableHours / weeklyBillableHoursTarget * 100.00;
 
   function authorizeFreshbooks() {
-    const redirectUri = Linking.makeUrl('/auth/redirect');
-
-    const config = {
-      serviceConfiguration: {
-        authorizationEndpoint: `https://auth.freshbooks.com/service/auth/oauth/authorize?client_id=FILL_IN&response_type=code`,
-        tokenEndpoint: 'https://api.freshbooks.com/auth/oauth/token'
-      },
-      redirectUrl: redirectUri,
-      clientId: 'FILL IN',
-      clientSecret: 'FILL IN',
-      scopes: [],
-    };
-
-    AppAuth.authAsync(config)
-    .then((authState) => {
-      AsyncStorage.setItem('@freshbooks_auth', JSON.stringify(authState)).then(() => {
-        console.log("Freshbooks auth stored!");
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+    
+    setLaunchingFreshbooksLogin(true);
+    freshbooks_api_client.authorize(() => {
+      checkFreshbooksAuth();
     });
   }
 
   function checkFreshbooksAuth() {
-    AsyncStorage.getItem('@freshbooks_auth')
-    .then((data) => {
-      if(data) {
-        setFreshbooksAuthCredentials(JSON.parse(data));
-      }
-      setCheckingFreshbooksAuth(false);
-    })
-    .catch(() => {
+    freshbooks_api_client.checkAuthorization(() => {
+      setFreshbooksAuthorized(freshbooks_api_client.authorized);
       setCheckingFreshbooksAuth(false);
     });
   }
 
   useEffect(() => {
-    if(freshbooksAuthCredentials) {
-      freshbooks_api_client.get('/test').then((response) => {
+    if(freshbooksAuthorized) {
+      freshbooks_api_client.get('/test', (response) => {
         console.log(response.data);
       });
     }
-  }, [freshbooksAuthCredentials]);
+  }, [freshbooksAuthorized]);
 
-  useEffect(checkFreshbooksAuth, []);
+  useEffect(() => {
+    checkFreshbooksAuth();
+  }, []);
+
   useEffect(() => {
     if(animation) {
       animation.play();
@@ -104,10 +82,12 @@ function WeeklyBillableHoursProgress() {
         checkingFreshbooksAuth ?
           <Spinner />
         :
-          freshbooksAuthCredentials ?
-            <Text>Freshbooks Authorized</Text>
-          :
-            <Button onPress={authorizeFreshbooks}>Authorize FreshBooks</Button>
+          !freshbooksAuthorized &&
+          <Button onPress={authorizeFreshbooks}>
+            {
+              launchingFreshbooksLogin ? 'Please wait...' : 'Authorize FreshBooks'
+            }
+          </Button>
       }
     </Card>
   );
--- src/utilities/freshbooks_api_client.ts
+++ src/utilities/freshbooks_api_client.ts
@@ -1,13 +1,85 @@
 import axios from 'axios';
 import environment from './environment';
+import * as AppAuth from 'expo-app-auth';
+import * as Linking from 'expo-linking';
+import * as SecureStore from 'expo-secure-store';
 
-const freshbooks_api_client = axios.create({
-  baseURL: `https://api.freshbooks.com/`,
-  headers: {
-    'Accept': 'application/json',
-    'Api-version': 'alpha',
-    'Authorization': 'Bearer FILL_IN'
+import apiClient from './api_client';
+
+const freshbooks_api_client = {
+  authorized: false,
+  accessToken: '',
+  checkAuthorization: function(onComplete) {
+
+    SecureStore.getItemAsync('freshbooks_auth')
+    .then((data) => {
+      if(data) {
+        const freshbooksAuthObject = JSON.parse(data);
+        this.authorized = true;
+        this.accessToken = freshbooksAuthObject.accessToken;
+        if(typeof onComplete === 'function') {
+          onComplete();
+        }
+      }
+    })
+
+    this.authorized = true;
   },
-});
+  authorize: function(onComplete) {
+    apiClient.get('/settings')
+    .then(({ data: settings }) => {
+      
+      let freshbooksApiClientId;
+      let freshbooksApiClientSecret;
+      settings.forEach((setting) => {
+        if(setting.name === 'freshbooks_api_client_id') {
+          freshbooksApiClientId = setting.value;
+        }
+        
+        if(setting.name === 'freshbooks_api_client_secret') {
+          freshbooksApiClientSecret = setting.value;
+        }
+      });
+      
+      const redirectUri = Linking.makeUrl('/auth/redirect');
+      const config = {
+        serviceConfiguration: {
+          authorizationEndpoint: `https://auth.freshbooks.com/service/auth/oauth/authorize?response_type=code`,
+          tokenEndpoint: 'https://api.freshbooks.com/auth/oauth/token'
+        },
+        redirectUrl: redirectUri,
+        clientId: freshbooksApiClientId,
+        clientSecret: freshbooksApiClientSecret,
+        scopes: [],
+      };
+
+      return AppAuth.authAsync(config);
+    })
+    .then((authState) => {
+      return SecureStore.setItemAsync('freshbooks_auth', JSON.stringify(authState))
+    })
+    .then(() => {
+      console.log("Freshbooks auth stored!");
+      if(typeof onComplete === 'function') {
+        onComplete();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  },
+  get: function(url, callback) {
+    const internal_freshbooks_api_client = axios.create({
+      baseURL: `https://api.freshbooks.com/`,
+      headers: {
+        'Accept': 'application/json',
+        'Api-version': 'alpha',
+        'Authorization': `Bearer ${this.accessToken}`
+      },
+    });
+
+    internal_freshbooks_api_client.get(url).then(callback);
+  }
+};
 
 export default freshbooks_api_client;
